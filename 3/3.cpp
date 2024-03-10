583bd358fc3dfe933531bb05b98a3838795f9d69
Компьютер <@DESKTOP-JR5U4KS>
2024-03-10 14:52:15 +0500

    Добавьте файлы проекта.

---
+++ 3/3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int Max, n;
+	std::cin >> Max;
+	for (int i = 1; i <= 6; i++) {
+		std::cin >> n;
+		if (n > Max) { Max = n; }
+	}
+	std::cout << Max;
+
+
+
+
+
+}
+
